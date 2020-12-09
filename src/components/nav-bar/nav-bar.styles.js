88ff5ab580e71ad6f8d194622c737ab2107e1b5c
paquitosoft <paquitosoftware@gmail.com>
2020-12-09 09:43:22 +0100

    NavBar responsive

---
+++ src/components/nav-bar/nav-bar.styles.js
@@ -0,0 +1,54 @@
+export default {
+	container: [
+		'bg-gray-900',
+		'sm:flex',
+		'sm:justify-between',
+		'sm:px-4',
+		'sm:py-3'
+	],
+	header: [
+		'flex',
+		'items-baseline',
+		'justify-between',
+		'px-4',
+		'py-3',
+		'sm:p-0'
+	],
+	logo: [
+		'h-8',
+		'w-auto'
+	],
+	buttonContainer: [
+		'sm:hidden'
+	],
+	button: [
+		'text-gray-500',
+		'focus:text-white',
+		'hover:text-white',
+		'focus:outline-none'
+	],
+	drawerIcon: [
+		'h-6',
+		'w-6',
+		'fill-current'
+	],
+	drawer: [
+		'px-2',
+		'pt-2',
+		'pb-4',
+		'sm:block',
+		'sm:flex'
+	],
+	drawerLink: [
+		'block',
+		'text-white',
+		'font-semibold',
+		'hover:bg-gray-800',
+		'rounded',
+		'px-2',
+		'py-1',
+		'sm:mt-0',
+		'sm:ml-6',
+		'sm:p-0'
+	]
+};
